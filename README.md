d596f7187496401b73ca27f742c7d49ec3610c3a
charlieSIGOGNEAU <charliesigogneau@gmail.com>
2024-10-03 12:39:14 +0200

    prmière save

---
+++ README.md
@@ -0,0 +1,1 @@
+l'exo sur la blockchain, j'ai beaucoup utilisé chatGPT, mais je pense que c'est interessant de savoir utiliser des fonction général comme max ou select plmlutot que le faire avec de boucle. je sais le faire avec les boucle. je l'ai fait pour l'exo big data
---
+++ lib/00_journalists.rb
@@ -0,0 +1,115 @@
+array=["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def nombrArray(array)
+  puts "#{array.length}"
+end
+
+def plusPetit(array)
+  plusCourtName=array[0]
+  array.each do |name|
+    if name.length<plusCourtName.length
+      plusCourtName=name
+    end
+  end
+  puts plusCourtName
+end
+
+# def tailPlusGrand (array)
+#   plusLongName=""
+#   array.each do |name|
+#     if name.length>plusLongName.length
+#     plusLongName=name
+#     end
+#   end
+#   puts plusLongName.length  
+# end
+
+def quantite5F(array)
+  quantite5=0
+  array.each do |name|
+    if name.length==6
+      quantite5+=1
+    end
+  end
+  puts quantite5
+end
+
+def quantitMajusculeF(array)
+  quantitMajuscule=0
+  array.each do |name|
+    if name[1]==name[1].upcase
+      quantitMajuscule+=1
+    end
+  end
+  puts quantitMajuscule
+end
+
+def trierAlphabetique(array)
+  array=array.sort
+  puts array
+end
+
+def trierTaille(array)
+  array=array.sort_by { |name| name.length }
+  puts array
+end
+
+def position(array)
+  puts "#{array.index("@epenser")}"
+end
+
+def classement(array)
+  # les classer
+  # pour taille -1
+  # comparer tail n et n+1
+  # si identiquer nom+=1
+  # si diferent il y a nombre de cette tail et nombre=1
+  array=array.sort_by { |name| name.length }
+  quantite=1
+  (array.length-1).times do |i|
+    if array[i].length==array[i+1].length
+      quantite+=1
+    else
+      puts " il y a #{quantite} noms de #{array[i].length} caracter"
+      quantite=1
+    end
+  end
+  if array[array.length-2]==array[array.length-1]
+    puts "il y a #{quantite} noms de #{array[array.length-1].length} caracter"
+  else
+    puts "il y a 1 nom de #{array[array.length-1].length} caracter"
+  end
+end
+
+def menu(array)
+  puts "1. Combien y a-t-il de handle dans cette array ?"
+  puts "2. Quel est le handle le plus court de cette liste ?"
+  puts "3. Combien y-a-t'il de handle contenant 5 caractères "
+  puts "4. Combien commencent par une majuscule ?"
+  puts "5. Trie la liste de handle par ordre alphabétique."
+  puts "6. Trie la liste de handle par taille des handle "
+  puts "7. Quelle est la position dans l'array de la personne @epenser ?"
+  puts "8. Sors-moi une répartition des handle par taille de ces derniers "
+  choi=gets.chomp.to_i
+  if choi==1
+    nombrArray(array)
+  elsif choi==2
+    plusPetit(array)
+  elsif choi==3
+    quantite5F(array)
+  elsif choi==4
+    quantitMajusculeF(array)
+  elsif choi==5
+    trierAlphabetique(array)
+  elsif choi==6
+    trierTaille(array)
+  elsif choi==7
+    position(array)
+  elsif choi==8
+    classement(array)
+  else
+    menu(array)
+  end
+end
+
+menu(array)
